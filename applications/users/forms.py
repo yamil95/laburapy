db64188ea2f38b0f774b1802c40c435846e84027
Yamil <yferru.9259@gmail.com>
2024-05-14 20:28:55 -0300

    [add][template_funcional][Esquema basico para trabajar, se customizo el modelo Users]

---
+++ applications/users/forms.py
@@ -0,0 +1,95 @@
+from django import forms
+from django.contrib.auth import authenticate
+#
+from .models import User
+
+class UserRegisterForm(forms.ModelForm):
+    
+    password1 = forms.CharField(
+        label='Contraseña',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Contraseña'
+            }
+        )
+    )
+    password2 = forms.CharField(
+        label='Contraseña',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Repetir Contraseña'
+            }
+        )
+    )
+
+    class Meta:
+        """Meta definition for Userform."""
+
+        model = User
+        fields = (
+            'email',
+            'full_name',
+            'profesion',
+            'genero',
+        )
+    
+    def clean(self):
+        cleaned_data = super(UserRegisterForm,self).clean()
+        if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+            self.add_error('password2', 'Las contraseñas no son iguales')
+        return cleaned_data
+
+class LoginForm(forms.Form):
+    email = forms.CharField(
+        label='E-mail',
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Correo Electronico',
+            }
+        )
+    )
+    password = forms.CharField(
+        label='Contraseña',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'contraseña'
+            }
+        )
+    )
+
+    def clean(self):
+        cleaned_data = super(LoginForm, self).clean()
+        email = self.cleaned_data['email']
+        password = self.cleaned_data['password']
+
+        if not authenticate(email=email, password=password):
+            raise forms.ValidationError('Los datos de usuario no son correctos')
+        
+        return cleaned_data
+
+
+
+class UpdatePasswordForm(forms.Form):
+
+    password1 = forms.CharField(
+        label='Contraseña',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Contraseña Actual'
+            }
+        )
+    )
+    password2 = forms.CharField(
+        label='Contraseña',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Contraseña Nueva'
+            }
+        )
+    )
